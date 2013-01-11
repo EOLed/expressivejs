44065fe4b8cbf9a7f5e12dfa802f6dab7bb48866
Amos Chan <amos.chan@chapps.org>
2013-01-11 17:46:44 -0500

    Dates populated and formatted with momentjs.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'), 
     posts = require('./routes/posts.js'), 
-    path = require('path');
+    path = require('path'),
+    moment = require('moment');
 
 var app = express();
 
@@ -11,6 +12,8 @@ app.configure(function() {
   app.use(express.bodyParser());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.locals.fromNow = function( d ) { return moment(d).fromNow() };
+  //todo add marked() so that we do not have to manipulate post.content
 });
 
 app.get('/', posts.index);
