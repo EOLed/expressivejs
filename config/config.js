621d9500356df700376775d48ddcb4c0c34e3614
Amos Chan <amosschan@gmail.com>
2013-08-06 20:27:02 -0400

    Delete everything

--- config/config.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-
-var config = JSON.parse(fs.readFileSync('./config/config.json'));
-config.site = JSON.parse(fs.readFileSync('./config/siteconfig.json'));
-module.exports = config;
